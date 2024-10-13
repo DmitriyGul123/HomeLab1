9532466c6d301d9cd2bc074bd9edaefe144ef3fa
Dmitriy Gulyakin <dimagulyakin177@gmail.com>
2024-10-13 14:53:55 +0300

    Add new update in hello.pas

--- hello.pas
+++ hello.pas
@@ -1,2 +1,12 @@
-﻿##
-Println('Hello World');
+﻿begin
+  Println('Choose your language: ');
+  
+  var number := ReadInteger('(1 - English, 2 - Spanish, 3 - Russian): ');
+  Assert(Number in 1..3, 'Incorrect! ');
+  
+  if number = 1 then
+    println('Hello!')
+  else if number = 2 then
+    println('Hola!')
+  else println('Здравствуйте!');
+end.
